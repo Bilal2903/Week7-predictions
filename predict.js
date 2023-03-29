e4ddc02af3636569e4ae74dfb2e28e9e62810ee4
Bilal <89836993+Bilal2903@users.noreply.github.com>
2023-03-29 14:19:55 +0200

    Done

--- predict.js
+++ predict.js
@@ -5,18 +5,20 @@ async function modelLoaded() {
     console.log("the model was successfully loaded!")
 }
 
-let button = document.getElementById('predict')
-button.addEventListener('click', ev => predict(ev))
+let predictButton = document.getElementById('predict')
+predictButton.addEventListener('click', ev => predict(ev))
 
 async function predict(ev) {
     let fixedAcid = document.getElementById('fixedAcid').value;
     let volatileAcid = document.getElementById('volatileAcid').value;
     let citricAcid = document.getElementById('citricAcid').value;
 
+    console.log(fixedAcid, volatileAcid, citricAcid)
+
     const result = await nn.predict({
-        fixed: parseInt(fixedAcid),
-        volatile: parseInt(volatileAcid),
-        citric:parseInt(citricAcid),
+        fixedAcidity: parseInt(fixedAcid),
+        volatileAcidity: parseInt(volatileAcid),
+        citricAcid:parseInt(citricAcid),
     })
     console.log(result)
 
