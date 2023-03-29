03a839d2c13736669fef9c1d98aea6ebe55c4a42
Bilal <89836993+Bilal2903@users.noreply.github.com>
2023-03-29 14:07:48 +0200

    Done

--- train.js
+++ train.js
@@ -1,5 +1,3 @@
-const nn = ml5.neuralNetwork({task: 'regression', debug: true})
-
 function loadData() {
         Papa.parse("./data/winequality.csv", {
                 download: true,
@@ -10,6 +8,8 @@ function loadData() {
 }
 
 function prepareData(data) {
+        const nn = ml5.neuralNetwork({task: 'regression', debug: true})
+
         data.sort(() => Math.random() > 0.5)
         let trainData = data.slice(0, Math.floor(data.length * 0.8))
         let testData  = data.slice(Math.floor(data.length * 0.8) + 1)
@@ -20,23 +20,7 @@ function prepareData(data) {
 
         nn.normalizeData()
         nn.train({ epochs: 32 }, () => doneTraining(nn))
-        predict();
 }
-
-async function predict(){
-        const result = await nn.predict({
-        volatileAcidity:7, fixedAcidity:4, citricAcid:0})
-        console.log("result")
-}
-
-// async function drawPredictions() {
-//         let predictions = []
-//         for (let hp = 40; hp <= 250; hp += 5) {
-//                 const prediction = await nn.predict({horsepower: hp})
-//                 predictions.push({x: hp, y: prediction[0].mpg})
-//         }
-//         updateChart("Predictions", predictions)
-// }
 
 function doneTraining(nn) {
         let saveButton = document.getElementById('saveButton')
